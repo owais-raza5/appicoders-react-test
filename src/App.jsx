0d7494de39d3b262de849ffe3db1b20edab88461
Owais Raza <owaisrazasamad@gmail.com>
2025-10-05 12:03:07 +0500

    dev: complete section three

--- src/App.jsx
+++ src/App.jsx
@@ -146,24 +146,183 @@ function App() {
       </section>
       <section className="section-three">
         <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-7">
-              <div className="s3tags-wrapper">
-                <div className="s3tag">iOS Development</div>
-                <div className="s3tag">Android Development</div>
-                <div className="s3tag">UI/UX Design</div>
-              </div>
-              <h2 className="sec3-heading">Caviar - Order Food Delivery</h2>
-              <p className="sec3-para">
-                Get Food delivered from curated lists of local restaurants right
-                at your fingertips with Caviar. Enjoy quality dining at home and
-                make any night a special occasion.
-              </p>
-              <img src={appstoreImg} alt="" />
-              <button className="sec3-btn">View Case Study</button>
-            </div>
-            <div className="col-lg-3">
-              <img src={sectionthreeImg} alt="" />
+          <div className="portfolio-slider-wrapper">
+            <Swiper
+              navigation={{
+                nextEl: ".portfolio-slider-next",
+                prevEl: ".portfolio-slider-prev",
+              }}
+              modules={[Navigation]}
+              loop={true}
+              className="portfolio-slider"
+              direction={"vertical"}
+              centeredSlides={true}
+              slidesPerView={"auto"}
+              spaceBetween={30}
+            >
+              <SwiperSlide className="portfolio-slide">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="portfolio-slide-content">
+                      <div className="s3tags-wrapper">
+                        <div className="s3tag">iOS Development</div>
+                        <div className="s3tag">Android Development</div>
+                        <div className="s3tag">UI/UX Design</div>
+                      </div>
+                      <h2 className="sec3-heading">
+                        Caviar - Order Food Delivery
+                      </h2>
+                      <p className="sec3-para">
+                        Get Food delivered from curated lists of local
+                        restaurants right at your fingertips with Caviar. Enjoy
+                        quality dining at home and make any night a special
+                        occasion.
+                      </p>
+                      <img src={appstoreImg} alt="" />
+                      <button className="sec3-btn">View Case Study</button>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex justify-content-center">
+                    <img
+                      className="portfolio-slider-img"
+                      src={sectionthreeImg}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="portfolio-slide">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="portfolio-slide-content">
+                      <div className="s3tags-wrapper">
+                        <div className="s3tag">iOS Development</div>
+                        <div className="s3tag">Android Development</div>
+                        <div className="s3tag">UI/UX Design</div>
+                      </div>
+                      <h2 className="sec3-heading">
+                        Caviar - Order Food Delivery
+                      </h2>
+                      <p className="sec3-para">
+                        Get Food delivered from curated lists of local
+                        restaurants right at your fingertips with Caviar. Enjoy
+                        quality dining at home and make any night a special
+                        occasion.
+                      </p>
+                      <img src={appstoreImg} alt="" />
+                      <button className="sec3-btn">View Case Study</button>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex justify-content-center">
+                    <img
+                      className="portfolio-slider-img"
+                      src={sectionthreeImg}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="portfolio-slide">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="portfolio-slide-content">
+                      <div className="s3tags-wrapper">
+                        <div className="s3tag">iOS Development</div>
+                        <div className="s3tag">Android Development</div>
+                        <div className="s3tag">UI/UX Design</div>
+                      </div>
+                      <h2 className="sec3-heading">
+                        Caviar - Order Food Delivery
+                      </h2>
+                      <p className="sec3-para">
+                        Get Food delivered from curated lists of local
+                        restaurants right at your fingertips with Caviar. Enjoy
+                        quality dining at home and make any night a special
+                        occasion.
+                      </p>
+                      <img src={appstoreImg} alt="" />
+                      <button className="sec3-btn">View Case Study</button>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex justify-content-center">
+                    <img
+                      className="portfolio-slider-img"
+                      src={sectionthreeImg}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="portfolio-slide">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="portfolio-slide-content">
+                      <div className="s3tags-wrapper">
+                        <div className="s3tag">iOS Development</div>
+                        <div className="s3tag">Android Development</div>
+                        <div className="s3tag">UI/UX Design</div>
+                      </div>
+                      <h2 className="sec3-heading">
+                        Caviar - Order Food Delivery
+                      </h2>
+                      <p className="sec3-para">
+                        Get Food delivered from curated lists of local
+                        restaurants right at your fingertips with Caviar. Enjoy
+                        quality dining at home and make any night a special
+                        occasion.
+                      </p>
+                      <img src={appstoreImg} alt="" />
+                      <button className="sec3-btn">View Case Study</button>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex justify-content-center">
+                    <img
+                      className="portfolio-slider-img"
+                      src={sectionthreeImg}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="portfolio-slide">
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="portfolio-slide-content">
+                      <div className="s3tags-wrapper">
+                        <div className="s3tag">iOS Development</div>
+                        <div className="s3tag">Android Development</div>
+                        <div className="s3tag">UI/UX Design</div>
+                      </div>
+                      <h2 className="sec3-heading">
+                        Caviar - Order Food Delivery
+                      </h2>
+                      <p className="sec3-para">
+                        Get Food delivered from curated lists of local
+                        restaurants right at your fingertips with Caviar. Enjoy
+                        quality dining at home and make any night a special
+                        occasion.
+                      </p>
+                      <img src={appstoreImg} alt="" />
+                      <button className="sec3-btn">View Case Study</button>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 d-flex justify-content-center">
+                    <img
+                      className="portfolio-slider-img"
+                      src={sectionthreeImg}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            <div className="portfolio-slider-nav">
+              <button className="portfolio-slider-prev">
+                <i className="fa-solid fa-arrow-left"></i>
+              </button>
+              <button className="portfolio-slider-next">
+                <i className="fa-solid fa-arrow-right"></i>
+              </button>
             </div>
           </div>
         </div>
